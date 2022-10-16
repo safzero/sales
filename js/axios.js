73567a25cba79a9dfe062a4cd1ad8a140e69991e
edusg83 <edusg83@gmail.com>
2022-10-16 17:27:14 +0200

    delete milestone

--- js/axios.js
+++ js/axios.js
@@ -1,5 +1,6 @@
 const urlMilestonesDelete = 'http://ligafalm.eu:28100/milestones/';
 const urlMilestones='http://ligafalm.eu:28100/milestones?page=0&size=100';
+var borrarMilestoneId=0;
 const headers = {
     'Content-Type':'application/json',
     'Access-Control-Allow-Origin': '*'
@@ -35,7 +36,7 @@ axios.get(urlMilestones,{headers})
     response.forEach(element => {
         let fechaI=new Date(element.start).toDateString();
         let fechaF = new Date(element.end).toDateString();
-        filas += `<tr><td>${element.name}</td><td>${fechaI}</td><td>${fechaF}</td><td>${element.progress}</td><td><i onclick="borrar(${element.id})" class='fas fa-trash fa-sm fa-fw mr-2 text-gray-400'></i></td></tr>`;
+        filas += `<tr><td>${element.name}</td><td>${fechaI}</td><td>${fechaF}</td><td>${element.progress}</td><td><i onclick="previoBorrar(${element.id})" data-toggle='modal' data-target='#deleteModal' class='fas fa-trash fa-sm fa-fw mr-2 text-gray-400'></i></td></tr>`;
     });
     tabla+=filas+finTabla;
     document.getElementById('cuerpo').innerHTML=tabla;
@@ -44,13 +45,28 @@ axios.get(urlMilestones,{headers})
     console.log(error)
 })
 
+function previoBorrar(milestonesId)
+{
+    borrarMilestoneId=milestonesId;
+}
+
 function borrar (milestonesId)
 {
-    axios.delete (urlMilestonesDelete+milestonesId,{headers})
-    .then((respuesta) => {
-        let response = respuesta.data;
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+    if (borrarMilestoneId>0)
+    {
+        axios.delete (urlMilestonesDelete+borrarMilestoneId,{headers})
+        .then((respuesta) => {
+            let response = respuesta.data;
+            console.log(response);
+            window.location.assign('http://localhost:8080/sales/milestones.html');
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+    else
+    {
+        console.log("No existe milestone");
+    }
+   
 }
